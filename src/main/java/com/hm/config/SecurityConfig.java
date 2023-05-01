8b6328f814b38da4304a529e72c7b066d476c1d6
HadisBtla <hadis.beitolamani@gmail.com>
2023-05-01 16:00:46 +0430

    create controller

--- src/main/java/com/hm/config/SecurityConfig.java
+++ src/main/java/com/hm/config/SecurityConfig.java
@@ -18,7 +18,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers("/").permitAll().anyRequest()
+                .antMatchers("/").permitAll().anyRequest().authenticated()
+                .and().formLogin().loginPage("/login");
     }
 
     @Autowired
